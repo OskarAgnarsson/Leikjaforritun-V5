510432dcae2328142020e670f5647209ee2c8eda
OskarAgnarsson <oskarag309@nemi.tskoli.is>
2019-04-15 02:15:55 +0000

    Byrjaði á uppsetningu galdra, er enn að vinna í því hvernig collision animation og collision eiga að virka

---
+++ Assets/Scripts/ProjectileManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileManager : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        StartCoroutine(ExplodeDelay());
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        anim.SetBool("Explode", true);
+        Destroy(this.gameObject, 1);
+    }
+
+    IEnumerator ExplodeDelay()
+    {
+        yield return new WaitForSeconds(4);
+        anim.SetBool("Explode", true);
+        Destroy(this.gameObject,1);
+    }
+}
---
+++ Assets/Scripts/SpellCast.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellCast : MonoBehaviour
+{
+    public Transform projectileSpawn;
+    public GameObject lightningBoltIdle;
+    public Rigidbody2D LightningBoltProjectile;
+    public GameObject fireballIdle;
+    public Rigidbody2D fireballProjectile;
+
+    private string[] spellSelect;
+    private bool allowFire = true;
+    private string spell;
+    private PlayerMovement playerMov;
+
+    void Start()
+    {
+        playerMov = GetComponent<PlayerMovement>();
+
+        spellSelect = new string[2];
+        spellSelect[0] = "Fireball";
+        spellSelect[1] = "LightningBolt";
+        spell = spellSelect[0];
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            if (spell == spellSelect[1])
+            {
+                ;
+            } else if (spell == spellSelect[1])
+            {
+                spell = spellSelect[0];
+                lightningBoltIdle.SetActive(false);
+                fireballIdle.SetActive(true);
+            }
+        }
+        else if (Input.GetKeyDown("2"))
+        {
+            if (spell == spellSelect[1])
+            {
+                ;
+            } else if (spell == spellSelect[0])
+            {
+                spell = spellSelect[1];
+                fireballIdle.SetActive(false);
+                lightningBoltIdle.SetActive(true);
+            }
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey("left"))
+        {
+            if (allowFire)
+            {
+                StartCoroutine(Cast(spell, "left"));
+            }
+        }
+        else if (Input.GetKey("right"))
+        {
+            if (allowFire)
+            {
+                StartCoroutine(Cast(spell, "right"));
+            }
+        }
+    }
+
+    IEnumerator Cast(string spell, string facing)
+    {
+        allowFire = false;
+        if (spell == spellSelect[0])
+        {
+            Rigidbody2D spellInstance = Instantiate(fireballProjectile, projectileSpawn.position, projectileSpawn.rotation);
+            if (facing=="left")
+            {
+                spellInstance.AddForce(new Vector2(-200, 0));
+            } else if (facing=="right")
+            {
+                spellInstance.MoveRotation(180);
+                spellInstance.AddForce(new Vector2(200, 0));
+            }
+
+        } else if (spell == spellSelect[1])
+        {
+            Rigidbody2D spellInstance = Instantiate(LightningBoltProjectile, projectileSpawn.position, projectileSpawn.rotation);
+            if (facing == "left")
+            {
+                spellInstance.AddForce(new Vector2(-400, 0));
+            }
+            else if (facing == "right")
+            {
+                spellInstance.MoveRotation(180);
+                spellInstance.AddForce(new Vector2(400, 0));
+            }
+        }
+        yield return new WaitForSeconds(0.35f);
+        allowFire = true;
+    }
+}
